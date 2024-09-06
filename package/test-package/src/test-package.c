9b9bb2126e06d76c2ef633815144d3dc89e00f4a
Daemondeal <daemondaeal@gmail.com>
2024-09-06 16:59:24 +0200

    fixed register ordering

--- package/test-package/src/test-package.c
+++ package/test-package/src/test-package.c
@@ -4,52 +4,52 @@
 #include <stdint.h>
 #include <sys/mman.h>
 
-int main(void) {
+// ANSI Colors
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+
+#define CRESET "\e[0m"
+
+volatile void *__global_ptrauth_device_base;
+
+uint64_t sign(uint64_t ptr, uint64_t tweak) {
+    *(uint64_t*)(__global_ptrauth_device_base + 0x10) = ptr;
+    *(uint64_t*)(__global_ptrauth_device_base + 0x18) = tweak;
+
+    return  *(uint64_t*)(__global_ptrauth_device_base + 0x20);
+}
+
+uint64_t auth(uint64_t ptr, uint64_t tweak) {
+    *(uint64_t*)(__global_ptrauth_device_base + 0x18) = tweak;
+    *(uint64_t*)(__global_ptrauth_device_base + 0x20) = ptr;
+
+    return  *(uint64_t*)(__global_ptrauth_device_base + 0x20);
+}
+
+void setup(void) {
     printf("Opening device\n");
     int fd = open("/dev/ptrauth", O_RDWR);
 
     printf("Memory Mapping\n");
-    volatile uint64_t *registers = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    __global_ptrauth_device_base = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+}
 
-    volatile uint64_t *plaintext  = registers + 0x10 / sizeof(uint64_t);
-    volatile uint64_t *tweak      = registers + 0x18 / sizeof(uint64_t);
-    volatile uint64_t *ciphertext = registers + 0x20 / sizeof(uint64_t);
+int main(void) {
+    setup();
 
-    uint64_t encrypted_pointer;
-    uint64_t decrypted_pointer;
-    uint64_t tmp;
     int pid = getpid();
 
-    uint64_t original_pointer = 0xABAB;
+    uint64_t ptr = 0x1234;
+    uint64_t signed_ptr = sign(ptr, 0x10);
+    printf("[%d] Signed pointer: %016llx\n", pid, signed_ptr);
 
-    // Tweak
-    *tweak = 0x10;
-
-    // ptr
-    *plaintext = original_pointer;
-
-    encrypted_pointer = *ciphertext;
-    tmp = *ciphertext;
-
-    printf("[%d] Original  pointer: %016llx\n", pid, original_pointer);
-    printf("[%d] Encrypted pointer: %016llx (second read: %016llx)\n", pid, encrypted_pointer, tmp);
     sleep(2);
+    uint64_t auth_ptr = auth(signed_ptr, 0x10);
 
-    *tweak = 0x10;
-    *ciphertext = encrypted_pointer;
-    decrypted_pointer = *ciphertext;
-    tmp = *ciphertext;
+    if (auth_ptr != ptr) {
+        printf(RED "[%d] Invalid auth pointer: %016llx\n" CRESET, auth_ptr, auth_ptr);
+    } else {
+        printf(GRN "[%d] Correct pointer!\n" CRESET, auth_ptr);
+    }
 
-    printf("[%d] Decrypted pointer: %016llx (second read: %016llx)\n", pid, decrypted_pointer, tmp);
-
-    encrypted_pointer += 0x10;
-    *tweak = 0x10;
-    *ciphertext = encrypted_pointer;
-    decrypted_pointer = *ciphertext;
-
-    printf("[%d] Modified encrypter pointer: %016llx\n", pid, encrypted_pointer);
-    printf("[%d] Decrypted modified encrypter pointer: %016llx\n", pid,  decrypted_pointer);
-
-    printf("Closing Device\n");
-    close(fd);
 }
